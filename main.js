02f9867e9148ed9e4db6d93fbd5309d29bf5b8ec
Regeneric <hkk@hkk-arch.localdomain>
2019-01-24 02:05:46 +0100

    Przygotowanie środowiska do pracy

---
+++ main.js
@@ -0,0 +1,51 @@
+/*--SETUP--*/
+const electron = require("electron");
+const path = require("path");
+const url = require("url");
+
+const {app, Menu, BrowserWindow} = electron;
+/*-!SETUP!-*/
+
+/*--PROPS--*/
+let mainWindow = null;
+    const main = {
+        width: 1280,
+        height: 720,
+        title: "Main Window",
+        show: false,
+        resizable: false
+    };  const mainProp = {
+            dir: __dirname,
+            file: "index.html",
+    };
+/*-!PROPS!-*/
+
+/*--RUN--*/
+app.on("ready", function() {
+    mainWindow = new BrowserWindow(main);
+        mainWindow = loadWindow(mainWindow, mainProp);
+
+    /*--EVENTS--*/
+    mainWindow.once("ready-to-show", function() {
+        mainWindow.show();
+        // mainWindow.setPosition(x, y);
+    });
+    mainWindow.once("close", function() {
+        mainWindow = null;
+        app.quit();
+    });
+    /*-!EVENTS!-*/
+
+    // Menu.setApplicationMenu(null);
+});
+
+function loadWindow(oWindow, oProp) {
+    this.oWindow = oWindow;
+    this.oWindow.loadURL(url.format({
+        pathname: path.join(oProp.dir, oProp.file),
+        protocol: "file:",
+        slashes: true
+    }));
+    return this.oWindow;
+}
+/*-!RUN!-*/
