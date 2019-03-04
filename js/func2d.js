05c52a6cbf21096f71cc6d4ff0357f809022cd68
Regeneric <hubek916@gmail.com>
2019-03-04 21:21:47 +0100

    chart.js

--- js/func2d.js
+++ js/func2d.js
@@ -8,4 +8,7 @@ const ctx = document.getElementById("func2d-chart").getContext("2d");
 const func2d = new Chart(ctx, {
     type: "line",
     data: [10, 20, 30, 40, 15, 13, 11],
+    options: {
+        responsive: false
+    }
 });
