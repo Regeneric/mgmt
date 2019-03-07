b731e660a5a1aa694716033ec054462023ac2462
Regeneric <hubek916@gmail.com>
2019-03-07 18:54:42 +0100

    Rysowanie podstawowych funkcji

--- js/func2d.js
+++ js/func2d.js
@@ -6,97 +6,20 @@ const chart = require("chart.js");
 const ctx = document.getElementById("func2d-chart").getContext("2d");
 
 const fxBtn = document.querySelector(".fx-btn");
-const fxInp = document.querySelector(".fx-inp");
+const fxInp = document.querySelectorAll(".fx-inp");
 /*-!SETUP!-*/
 
 /*--PROPS--*/
 let fxValue = 0;
-const regExes = [{
-        id: 0,
-        name: "linearFx",
-        fx: /^([0-9]{1,11}|a)(\*x|x)(\+|-)([0-9]{1,11}|b)$/g
-        // ax+b
-    }, {
-        id: 1,
-        name: "linearFxShort",
-        fx: /^x(\+|-)([0-9]{1,11}|b)$/g
-        // x+b
-    }, {
-        id: 2,
-        name: "genSquareFx",
-        fx: /^([0-9]{1,6}|a)(\*x|x)\^2(\+|-)([0-9]{1,6}|b)(\*x|x)(\+|-)([0-9]{1,6}|c)$/g
-        // ax^2+bx+c
-    }, {
-        id: 3,
-        name: "genSquareFxShort",
-        fx: /^x\^2$/g
-        // x^2
-    }, {
-        id: 4,
-        name: "ratSquareFx",
-        fx: /^([0-9]{1,5}|a)(\(|\*\()x(\+|-)([0-9]{1,5}|b)\)(\(|\*\()x(\+|-)([0-9]{1,6}|c)\)$/g
-        // a(x-b)(x-c)
-    }, {
-        id: 5,
-        name: "canSquareFx",
-        fx: /^([0-9]{1,6}|a)(\*\(|\()x(\+|-)([0-9]{1,6}|p)\)\^2(\+|-)([0-9]{1,6}|q)$/g
-        // a(x-p)^2+q
-    }, {
-        id: 6,
-        name: "absValueFx",
-        fx: /^([0-9]{1,6}|a)(\*\||\|)x(\+|-)([0-9]{1,6}|p)\|(\+|-)([0-9]{1,7}|q$)/g
-        // a|x-p|+q
-    }, {
-        id: 7,
-        name: "homoFx",
-        fx: /^([0-9]{1,7}|a)\/\(x(\+|-)([0-9]{1,7}|p)\)(\+|-)([0-9]{1,7}|q)$/g
-        // a/(x-p)+q
-    }, {
-        id: 8,
-        name: "homoFxShort",
-        fx: /^([0-9]{1,20}|a)\/x$/g
-        // a/x
-    }, {
-        id: 9,
-        name: "thirdPolyFx",
-        fx: /^([0-9]{1,3}|a)(\*x|x)\^3(\+|-)([0-9]{1,3}|b)(\*x|x)\^2(\+|-)([0-9]{1,4}|c)(\*x|x)(\+|-)([0-9]{1,4}|p)/g
-        // ax^3+bx^2+cx+p
-    }
-];
-/*-!PROPS!-*/
+let a = b = c = p = q = 1;
+let func2d = null;
 
-/*--RUN--*/
-/*--EVENTS--*/
-fxBtn.addEventListener("click", () => {
-    fxValue = fxInp.value
-    let flag = 0;
-    regExes.forEach(r => {
-        if (r.fx.test(fxValue)) flag = 1;
-    });
-
-    if (flag) {
-        console.log(fxValue);
-        flag = 0;
-    } else console.log("Debilu ujebany, popraw to");
-});
-/*-!EVENTS!-*/
-
-function ctg(x) {return 1/Math.tan(x);}
-function f(x) {
-    return 2*x+1;
-}
-
-const data = new Array();
-for (let x = -15; x < 15; x++) {
-    data.push({x:x,y:f(x)});
-}
-
-
-const func2d = new Chart(ctx, {
+let data = new Array();
+const chartProps = {
     type: "line",
     data: {
         datasets: [{
-            label: "2x+1",
+            label: '',
             borderColor: 'rgb(255, 99, 132)',
             data: data
         }]
@@ -109,5 +32,103 @@ const func2d = new Chart(ctx, {
             }]
         }
     }
-});
+};
+
+const regExes = [{
+        id: 0,
+        name: "linearFx",
+        fx: /^([0-9]{1,11}|a)\*x(\+|\-)([0-9]{1,11}|b)$/
+        // a*x+b
+    }, {
+        id: 1,
+        name: "linearFxShort",
+        fx: /^x(\+|\-)([0-9]{1,11}|b)$/
+        // x+b
+    }, {
+        id: 2,
+        name: "genSquareFx",
+        fx: /^([0-9]{1,6}|a)\*x\^2(\+|\-)([0-9]{1,6}|b)\*x(\+|\-)([0-9]{1,6}|c)$/
+        // a*x^2+b*x+c
+    }, {
+        id: 3,
+        name: "genSquareFxShort",
+        fx: /^x\^2$/
+        // x^2
+    }, {
+        id: 4,
+        name: "ratSquareFx",
+        fx: /^([0-9]{1,5}|a)\*\(x(\+|\-)([0-9]{1,5}|b)\)\*\(x(\+|\-)([0-9]{1,6}|c)\)$/
+        // a*(x-b)*(x-c)
+    }, {
+        id: 5,
+        name: "canSquareFx",
+        fx: /([0-9]{1,6}|a)\*\(x(\+|\-)([0-9]{1,6}|p)\)\^2(\+|\-)([0-9]{1,6}|q)$/
+        // a*(x-p)^2+q
+    }, {
+        id: 6,
+        name: "absValueFx",
+        fx: /^([0-9]{1,6}|a)\*\|x(\+|\-)([0-9]{1,6}|p)\|(\+|\-)([0-9]{1,7}|q)$/
+        // a*|x-p|+q
+    }, {
+        id: 7,
+        name: "homoFx",
+        fx: /^([0-9]{1,7}|a)\/\(x(\+|\-)([0-9]{1,7}|p)\)(\+|\-)([0-9]{1,7}|q)$/
+        // a/(x-p)+q
+    }, {
+        id: 8,
+        name: "homoFxShort",
+        fx: /^([0-9]{1,20}|a)\/x$/
+        // a/x
+    }, {
+        id: 9,
+        name: "thirdPolyFx",
+        fx: /^([0-9]{1,3}|a)\*x\^3(\+|\-)([0-9]{1,3}|b)\*x\^2(\+|\-)([0-9]{1,4}|c)\*x(\+|-)([0-9]{1,4}|p)$/
+        // a*x^3+b*x^2+c*x+p
+    }
+];
+/*-!PROPS!-*/
+
+/*--RUN--*/
+/*--EVENTS--*/
+fxBtn.addEventListener("click", () => {
+    let rightX = fxInp[0].value;
+    let leftX = ~rightX+1; 
+
+    fxValue = fxInp[1].value
+    func2d.destroy();
+    data.length = 0;
+    regExes.forEach(r => {
+        if (r.fx.test(fxValue)) {
+            switch(r.id) {
+                // Zwykłe funkcje
+                case 0:
+                case 1:
+                case 4:
+                case 5:
+                case 7:
+                case 8:
+                    fxValue = r.fx.exec(fxValue)[0];
+
+                    function f(x) {
+                        return eval(fxValue);
+                    }
+
+                    for (let x = leftX; x < rightX; x++) {
+                        data.push({x:x,y:f(x)});
+                    } break;     
+                case 2:
+                case 3:
+                case 6:
+                case 9:
+                    break;
+            }
+        }
+    });
+
+    func2d = new Chart(ctx, chartProps);
+    func2d.data.datasets[0].label = fxValue.toString();
+    func2d.update();
+}); 
+/*-!EVENTS!-*/
+func2d = new Chart(ctx, chartProps);
 /*-!RUN!-*/
