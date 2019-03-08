5db1bf867f8005707f741db7831753c62d3cca39
Dragroth <werander.k@gmail.com>
2019-03-08 20:04:44 +0100

    Dodano obsługę klawiatury.

--- js/base.js
+++ js/base.js
@@ -16,6 +16,7 @@ const maxChar = 15; // Max number of characters you can input
 // Mouse 0-9
 baseButtons.forEach(baseButton => {
     baseButton.addEventListener("click", () => {
+        //Continue inserting numbers after using calculate button
         if (resultBox.className == "highlited") {
             resultBox.className = "unhighlited";
             inputBox.className = "highlited";
@@ -32,7 +33,7 @@ baseButtons.forEach(baseButton => {
                 //0 in other places
                 if (inputBox.firstChild.nodeValue.match(/\d+/g).map(Number)[inputBox.firstChild.nodeValue.match(/\d+/g).map(Number).length-1] == 0 ) {
                     if (/[0-9]{0,}\.[0-9]{0,}$/g.test(inputBox.firstChild.nodeValue)) inputBox.firstChild.nodeValue += 0;
-            }
+                }
                 else inputBox.firstChild.nodeValue += 0;
             }
             
@@ -45,7 +46,7 @@ baseButtons.forEach(baseButton => {
             }
         }
         //Update resultbox
-            resultBox.firstChild.nodeValue = "= " + eval(inputBox.firstChild.nodeValue);
+        resultBox.firstChild.nodeValue = "= " + eval(inputBox.firstChild.nodeValue);
     });
 });
 
@@ -122,68 +123,112 @@ calc.forEach(calc => {
 
 
 //Keyboard support
-// document.addEventListener("keyup", () => {
-//     console.log(event.key);
+document.addEventListener("keyup", () => {
 
-//     if (inputBox.firstChild.nodeValue.length <= maxChar) {
+    console.log(event.key);
+    //Continue inserting numbers after using calculate button
+    if (resultBox.className == "highlited") {
+        resultBox.className = "unhighlited";
+        inputBox.className = "highlited";
+        inputBox.firstChild.nodeValue = ' ';
+    }
 
-//         if (!(isNaN(event.key))){
-//             if (event.key == "0"){
-//                 if (inputBox.firstChild.nodeValue == " "){
-//                     inputBox.firstChild.nodeValue = 0;
-//                 }
-//                 if (inputBox.firstChild.nodeValue.match(/\d+/g).map(Number)[inputBox.firstChild.nodeValue.match(/\d+/g).map(Number).length-1] == 0 ) {
-//                     if (/[0-9]{0,}\.[0-9]{0,}$/g.test(inputBox.firstChild.nodeValue)) inputBox.firstChild.nodeValue += 0;
-//             }
-//                 else inputBox.firstChild.nodeValue += 0;
-//             }
+    //Max char number
+    if (inputBox.firstChild.nodeValue.length <= maxChar) {
+
+        //Inserting numbers
+        if (!(isNaN(event.key))){
+            // Insert 0
+            if (event.key == "0"){
+                //0 at the start of a number
+                if (inputBox.firstChild.nodeValue == " "){
+                    inputBox.firstChild.nodeValue = 0;
+                }
+                //0 in other places
+                if (inputBox.firstChild.nodeValue.match(/\d+/g).map(Number)[inputBox.firstChild.nodeValue.match(/\d+/g).map(Number).length-1] == 0 ) {
+                    if (/[0-9]{0,}\.[0-9]{0,}$/g.test(inputBox.firstChild.nodeValue)) inputBox.firstChild.nodeValue += 0;
+                }
+                else inputBox.firstChild.nodeValue += 0;
+            }
             
 
-//             // Insert 1-9
-//             for (let i = 1; i < 10; i++) {
-//                 if (event.key == i && inputBox.firstChild.nodeValue !== '0') {
-//                     inputBox.firstChild.nodeValue += i;
-//                 }
-//             }
-//         }
-//     switch(event.key){
-//         case "+": {
-//             inputBox.firstChild.nodeValue += "+";
-//             break;
-//         }
-//         case "-": {
-//             inputBox.firstChild.nodeValue += "-";
-//             break;
-//         }
-//         case "*": {
-//             inputBox.firstChild.nodeValue += "*";
-//             break;
-//         }
-//         case "/": {
-//             inputBox.firstChild.nodeValue += "/";
-//             break;
-//         }
-//         case ".": {
-//             inputBox.firstChild.nodeValue += ".";
-//             break;
-//         }
-//         case "Backspace": {
-//             inputBox.firstChild.nodeValue = inputBox.firstChild.nodeValue.slice(0, -1);
-//             if ( !(isNaN(inputBox.firstChild.nodeValue[inputBox.firstChild.nodeValue.length -1])) ) {
-//                 resultBox.firstChild.nodeValue = "= " + eval(inputBox.firstChild.nodeValue);
-//             }
-//             break;
-//         }
-//         case "Escape": {
-//             inputBox.firstChild.nodeValue = " "
-//             resultBox.firstChild.nodeValue = "= "
-//             break;
-//         }
+            // Insert 1-9
+            for (let i = 1; i < 10; i++) {
+                if (event.key == i && inputBox.firstChild.nodeValue !== '0') {
+                    inputBox.firstChild.nodeValue += i;
+                }
+            }
+            //Update resultbox
+            resultBox.firstChild.nodeValue = "= " + eval(inputBox.firstChild.nodeValue);
+        }
+    }
+        //Keyboard operations
+        //Continue after using equal button
+         if (resultBox.className == "highlited" && event.key != "Enter" && event.key != "Backspace"){
+            resultBox.className = "unhighlited";
+            inputBox.className = "highlited";
+            inputBox.firstChild.nodeValue = resultBox.firstChild.nodeValue.substr(2);
+        }
 
-//     }
+        //Equal button
+        if (event.key == "Enter" && resultBox.className == "unhighlited") {
+            resultBox.className = "highlited";
+            ipcRenderer.send("inputBox", "base" + inputBox.firstChild.nodeValue);
+            inputBox.className = "unhighlited";
+        }
 
-//     }
+        //Calculating buttons
+        if (!(isNaN(inputBox.firstChild.nodeValue[inputBox.firstChild.nodeValue.length -1])) && inputBox.firstChild.nodeValue != ' ') {
+            switch(event.key) {
+                case "+": {
+                    inputBox.firstChild.nodeValue += "+";
+                    break;
+                }
+                case "-": {
+                    inputBox.firstChild.nodeValue += "-";
+                    break;
+                }
+                case "*": {
+                    inputBox.firstChild.nodeValue += "*";
+                    break;
+                }
+                case "/": {
+                    inputBox.firstChild.nodeValue += "/";
+                    break;
+                }
+                case ".": {
+                    if (!(/[0-9]{0,}\.[0-9]{0,}$/g.test(inputBox.firstChild.nodeValue))){
+                    inputBox.firstChild.nodeValue += ".";
+                    }
+                    break;
+                }
+            }
+        }
 
-// });
+        //Backspace button
+        if (event.key == "Backspace"){
+
+            if (inputBox.firstChild.nodeValue != " ") {
+            inputBox.firstChild.nodeValue = inputBox.firstChild.nodeValue.slice(0, -1);
+            }
+
+            if ( !(isNaN(inputBox.firstChild.nodeValue[inputBox.firstChild.nodeValue.length -1]))) {
+                if (inputBox.firstChild.nodeValue.length > 1 ) {
+                    resultBox.firstChild.nodeValue = "= " + eval(inputBox.firstChild.nodeValue);
+                } else resultBox.firstChild.nodeValue = "= "
+
+            }
+
+        }
+
+        //C button
+        if (event.key == "Escape"){
+            inputBox.firstChild.nodeValue = " ";
+            resultBox.firstChild.nodeValue = "= ";
+        }
+
+    
+
+});
 /*-!EVENTS!-*/ 
 /*-!RUN!-*/
