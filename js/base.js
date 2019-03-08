fab661ffcd837e03463d7f644d1a5c5b292869c0
Dragroth <werander.k@gmail.com>
2019-03-08 16:43:40 +0100

    Poprawki w działaniu, początek obsługi klawiatury

--- js/base.js
+++ js/base.js
@@ -4,7 +4,6 @@ const resultBox = document.querySelector("#resultBox");
 
 const baseButtons = document.querySelectorAll(".baseButton");
 const calc = document.querySelectorAll(".calc");
-const equals = document.querySelector(".equals");
 /*-!SETUP!-*/
 
 /*--PROPS--*/
@@ -13,6 +12,7 @@ const maxChar = 12; // Max number of characters you can input
 
 /*--RUN--*/
 /*--EVENTS--*/
+
 // Mouse 0-9
 baseButtons.forEach(baseButton => {
     baseButton.addEventListener("click", () => {
@@ -42,9 +42,7 @@ baseButtons.forEach(baseButton => {
                 }
             }
         }
-        if ( !(isNaN(inputBox.firstChild.nodeValue[inputBox.firstChild.nodeValue.length -1])) ) {
             resultBox.firstChild.nodeValue = "= " + eval(inputBox.firstChild.nodeValue);
-        }
     });
 });
 
@@ -79,24 +77,99 @@ calc.forEach(calc => {
                     inputBox.firstChild.nodeValue += ".";
                     break;
                 }
-                case "btnbackspace": {
-                    inputBox.firstChild.nodeValue = inputBox.firstChild.nodeValue.slice(0, -1);
-                    break;
-                }
-                case "btnc": {
-                    inputBox.firstChild.nodeValue = " "
-                    resultBox.firstChild.nodeValue = "= "
-                    break;
-                }
             }
+        }
+
+        if (calc.id == "btnbackspace"){
+
+            if (inputBox.firstChild.nodeValue != " ") {
+            inputBox.firstChild.nodeValue = inputBox.firstChild.nodeValue.slice(0, -1);
+            }
+
+            if ( !(isNaN(inputBox.firstChild.nodeValue[inputBox.firstChild.nodeValue.length -1]))) {
+                if (inputBox.firstChild.nodeValue.length > 1 ) {
+                    resultBox.firstChild.nodeValue = "= " + eval(inputBox.firstChild.nodeValue);
+                } else resultBox.firstChild.nodeValue = "= "
+
+            }
+
+        }
+        if (calc.id == "btnc"){
+            inputBox.firstChild.nodeValue = " ";
+            resultBox.firstChild.nodeValue = "= ";
+        }
+        if (calc.id == "btnequals"){
+            inputBox.className = "unhighlited";
+            ipcRenderer.send("inputBox", "base" + inputBox.firstChild.nodeValue);
+            resultBox.className = "highlited";
         }
     });
 });
 
-equals.addEventListener("click", () => {
-    inputBox.className = "unhighlited";
-    ipcRenderer.send("inputBox", "base" + inputBox.firstChild.nodeValue);
-    resultBox.className = "highlited";
-});
+
+//Keyboard support
+// document.addEventListener("keyup", () => {
+//     console.log(event.key);
+
+//     if (inputBox.firstChild.nodeValue.length <= maxChar) {
+
+//         if (!(isNaN(event.key))){
+//             if (event.key == "0"){
+//                 if (inputBox.firstChild.nodeValue == " "){
+//                     inputBox.firstChild.nodeValue = 0;
+//                 }
+//                 if (inputBox.firstChild.nodeValue.match(/\d+/g).map(Number)[inputBox.firstChild.nodeValue.match(/\d+/g).map(Number).length-1] == 0 ) {
+//                     if (/[0-9]{0,}\.[0-9]{0,}$/g.test(inputBox.firstChild.nodeValue)) inputBox.firstChild.nodeValue += 0;
+//             }
+//                 else inputBox.firstChild.nodeValue += 0;
+//             }
+            
+
+//             // Insert 1-9
+//             for (let i = 1; i < 10; i++) {
+//                 if (event.key == i && inputBox.firstChild.nodeValue !== '0') {
+//                     inputBox.firstChild.nodeValue += i;
+//                 }
+//             }
+//         }
+//     switch(event.key){
+//         case "+": {
+//             inputBox.firstChild.nodeValue += "+";
+//             break;
+//         }
+//         case "-": {
+//             inputBox.firstChild.nodeValue += "-";
+//             break;
+//         }
+//         case "*": {
+//             inputBox.firstChild.nodeValue += "*";
+//             break;
+//         }
+//         case "/": {
+//             inputBox.firstChild.nodeValue += "/";
+//             break;
+//         }
+//         case ".": {
+//             inputBox.firstChild.nodeValue += ".";
+//             break;
+//         }
+//         case "Backspace": {
+//             inputBox.firstChild.nodeValue = inputBox.firstChild.nodeValue.slice(0, -1);
+//             if ( !(isNaN(inputBox.firstChild.nodeValue[inputBox.firstChild.nodeValue.length -1])) ) {
+//                 resultBox.firstChild.nodeValue = "= " + eval(inputBox.firstChild.nodeValue);
+//             }
+//             break;
+//         }
+//         case "Escape": {
+//             inputBox.firstChild.nodeValue = " "
+//             resultBox.firstChild.nodeValue = "= "
+//             break;
+//         }
+
+//     }
+
+//     }
+
+// });
 /*-!EVENTS!-*/ 
 /*-!RUN!-*/
